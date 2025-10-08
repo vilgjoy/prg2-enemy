ee337c915be4fcb6f0fedf77faf841528b55a902
¨Vilgot <vilgot.sandberg@elev.ga.ntig.se>
2025-10-08 13:30:01 +0200

    la till en move function som förflytter enemy i game_map

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+game_map = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
 class Enemy:
-    def __init__(self, health: int, attack_power: int, name: str):
+    def __init__(self, health: int, attack_power: int, name: str, x: int, y: int):
         self.health = health
         self.attack_power = attack_power
         self.name = name
+        self.x = x
+        self.y = y
 
     def print_status(self):
         print(f"{self.name} - hälsa: {self.health}, attack: {self.attack_power}")
@@ -21,18 +31,20 @@ class Enemy:
             print("du är död, noob")
         else:
             print("du är inte död än")
+    
+    def move(self, directionx, directiony, game_map):
+        new_x = self.x + directionx
+        new_y = self.y + directiony
+        if (0 <= new_y < len(game_map)) and (0 <= new_x < len(game_map[0])) and (game_map[new_y][new_x] == 0):
+            game_map[self.y][self.x] = 0
+            game_map[new_y][new_x] = 1
+            self.x = new_x
+            self.y = new_y
+            print(f"{self.name} flyttade till position ({self.x}, {self.y})")
+        else:
+            print(f"{self.name} kan inte flytta dit vro")
 
 
-
-gobo = Enemy(100, 10, "Gobo")
-gobo.print_status()
-slime = Enemy(100, 5, "Slimey")
-slime.print_status()
-while True:
-    slime.attack(gobo)
-    gobo.attack(slime)
-    slime.check_death()
-    gobo.check_death()
-    if gobo.health <= 0 or slime.health <= 0:
-        break
-slime.check_death()
+gobo = Enemy(100, 10, "Gobo", 0, 0)
+gobo.move(1, 2, game_map)
+gobo.move(1, 1, game_map)
